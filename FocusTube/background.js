2015198ac84ea82c76667f6e495acbb98fd763c6
Poorvith T A <207068171+PoorvithTA@users.noreply.github.com>
2026-02-22 18:43:52 +0530

    Delete FocusTube directory

--- FocusTube/background.js
+++
@@ -1,8 +0,0 @@
-chrome.commands.onCommand.addListener((command) => {
-  if (command === "toggle-focus") {
-    chrome.storage.sync.get(["enabled"], (res) => {
-      const newState = !res.enabled;
-      chrome.storage.sync.set({ enabled: newState });
-    });
-  }
-});
